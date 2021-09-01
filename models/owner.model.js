f05410d480efe4b01cf3c160858afa16277b041d
Manuel Castillo <m.castillo@gestionescastillo.com>
2021-09-01 21:04:39 +0200

    updated ownersApi

--- models/owner.model.js
+++ models/owner.model.js
@@ -4,6 +4,11 @@ const getAll = () => {
     return executeQuery('select * from owners');
 }
 
+const getById = (id) => {
+    return executeQuery('select * from owners where owners.id = ?', [id]);
+}
+
 module.exports = {
-    getAll
+    getAll,
+    getById
 }
--- routes/api/owners.js
+++ routes/api/owners.js
@@ -1,9 +1,14 @@
 let router = require('express').Router();
-const { getAll } = require('../../models/owner.model')
+const { getAll, getById } = require('../../models/owner.model')
 
 router.get('/', async (req, res) => {
     let result = await getAll()
     res.json(result);
 });
+
+router.get('/:id', async (req, res) => {
+    let result = await getById(req.params.id);
+    res.json(result)
+})
 
 module.exports = router
