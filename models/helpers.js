03d81bcbab23e7e9da74676f4eb30904e39d0a44
Manuel Castillo <m.castillo@gestionescastillo.com>
2021-09-02 17:59:20 +0200

    updated helpers

--- models/helpers.js
+++ models/helpers.js
@@ -4,6 +4,7 @@ const executeQuery = (sql, arrValues = []) => {
         db.query(
             sql, arrValues, (err, result) => {
                 if (err) reject(err);
+                if (result.length === 0) resolve({ response: 'No se han encontrado registros' });
                 resolve(result);
             }
         )
@@ -17,7 +18,7 @@ const executeQueryUnique = (sql, arrValues = []) => {
             arrValues,
             (err, result) => {
                 if (err) reject(err);
-                if (result.length !== 1) resolve(null);
+                if (result.length !== 1) resolve({ response: 'No se han encontrado registros' });
                 resolve(result[0]);
             }
         )
