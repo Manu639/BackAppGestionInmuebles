853de67fe3ae31f960e14affd9aed7e1684c0cbc
Manuel Castillo <m.castillo@gestionescastillo.com>
2021-09-05 00:11:08 +0200

    fixed query on properties model

--- models/properties.model.js
+++ models/properties.model.js
@@ -1,15 +1,15 @@
-const { executeQuery } = require("./helpers");
+const { executeQuery, executeQueryUnique } = require("./helpers");
 
 const getAll = () => {
     return executeQuery('select * from properties');
 }
 
 const getById = (id) => {
-    return executeQuery('select * from properties where properties.id = ?', [id]);
+    return executeQueryUnique('select * from properties where properties.id = ?', [id]);
 }
 
 const getByType = (typeId) => {
-    return executeQueryUnique('select * from properties where properties.type = ?', [typeId]);
+    return executeQuery('select * from properties where properties.type = ?', [typeId]);
 }
 
 module.exports = {
