d11d5e5e656a1195eddcb7399b1272405fe6532f
Manuel Castillo <m.castillo@gestionescastillo.com>
2021-09-04 21:09:08 +0200

    Update owner query

--- models/owner.model.js
+++ models/owner.model.js
@@ -8,7 +8,22 @@ const getById = (id) => {
     return executeQueryUnique('select * from owners where owners.id = ?', [id]);
 }
 
+const update = (owner) => {
+
+    let query = 'UPDATE owners SET ';
+
+    for (property in owner) {
+        query += `${property} = '${owner[property]}', `
+    };
+
+    query = query.substring(0, query.length - 2);
+    query += ` WHERE owners.id = ${owner.id};`;
+
+    return executeQuery(query)
+}
+
 module.exports = {
     getAll,
-    getById
+    getById,
+    update
 }
--- routes/api/owners.js
+++ routes/api/owners.js
@@ -1,5 +1,5 @@
 let router = require('express').Router();
-const { getAll, getById } = require('../../models/owner.model')
+const { getAll, getById, update } = require('../../models/owner.model')
 
 router.get('/', async (req, res) => {
     let result = await getAll()
@@ -8,6 +8,11 @@ router.get('/', async (req, res) => {
 
 router.get('/:id', async (req, res) => {
     let result = await getById(req.params.id);
+    res.json(result)
+})
+
+router.post('/update', async (req, res) => {
+    let result = await update(req.body);
     res.json(result)
 })
 
